2be96a06ff97e6ce55a2724b2d7e97eb4657d8e8
Rytis <rytis.augustauskas1@gmail.com>
2021-05-20 21:55:18 +0300

    confusion matrix

--- train.py
+++ train.py
@@ -12,6 +12,8 @@ from tensorflow.keras.layers import Conv1D, MaxPooling1D, LeakyReLU, BatchNormal
 from tensorflow.keras.optimizers import Adam
 import scipy.io.wavfile
 from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
 # Data
 # train
@@ -189,6 +191,83 @@ class data_flow(Sequence):
         return x, y
 
 
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+    """
+    given a sklearn confusion matrix (cm), make a nice plot
+
+    Arguments
+    ---------
+    cm:           confusion matrix from sklearn.metrics.confusion_matrix
+
+    target_names: given classification classes such as [0, 1, 2]
+                  the class names, for example: ['high', 'medium', 'low']
+
+    title:        the text to display at the top of the matrix
+
+    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                  see http://matplotlib.org/examples/color/colormaps_reference.html
+                  plt.get_cmap('jet') or plt.cm.Blues
+
+    normalize:    If False, plot the raw numbers
+                  If True, plot the proportions
+
+    Usage
+    -----
+    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
+                                                              # sklearn.metrics.confusion_matrix
+                          normalize    = True,                # show proportions
+                          target_names = y_labels_vals,       # list of names of the classes
+                          title        = best_estimator_name) # title of graph
+
+    Citiation
+    ---------
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    """
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import itertools
+
+    accuracy = np.trace(cm) / np.sum(cm).astype('float')
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()
+
+
 def make_confusion_matrix(model, test_generator):
     print('Predicting...')
     true_labels = test_generator.get_labels()
@@ -196,10 +275,25 @@ def make_confusion_matrix(model, test_generator):
     Y_pred = model.predict(test_generator)
     y_pred = np.argmax(Y_pred, axis=1)
     print('Confusion Matrix')
-    print(confusion_matrix(y_true, y_pred))
+    conf_matrix = confusion_matrix(y_true, y_pred)
+    plot_confusion_matrix(conf_matrix, test_generator.class_names)
+    print(conf_matrix)
+   """ fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(conf_matrix)
+    plt.title('Confusion matrix of the classifier')
+    fig.colorbar(cax)
+    ax.xaxis.set_major_locator(MultipleLocator(1));
+    ax.yaxis.set_major_locator(MultipleLocator(1))
+    ax.set_xticklabels(test_generator.class_names)
+
+    ax.set_yticklabels(test_generator.class_names)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.show()
     print(classification_report(y_true, y_pred, target_names=test_generator.class_names))
     print('names:')
-    print(test_generator.class_names)
+    print(test_generator.class_names)"""
 
 
 def train():
